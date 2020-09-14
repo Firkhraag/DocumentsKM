797dae37b6b3ff2d7c4dcf305b3042908db670cc
Firkhraag <firkhraag@gmail.com>
2020-09-14 08:49:42 +0500

    Update server

---
+++ server/DocumentsKM/Tests/MarksControllerTest.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using DocumentsKM.Data;
+using Moq;
+using Xunit;
+
+namespace ProjectKM.Tests
+{
+    public class MarksControllerTest
+    {
+
+        private readonly Mock<IMarkRepo> _repo;
+
+        // [Fact]
+        // public async Task Add_to_cart_success()
+        // {
+        //     //Arrange
+        //     var fakeCatalogItem = GetFakeCatalogItem();
+
+        //     _basketServiceMock.Setup(x => x.AddItemToBasket(It.IsAny<ApplicationUser>(), It.IsAny<Int32>()))
+        //         .Returns(Task.FromResult(1));
+
+        //     //Act
+        //     var orderController = new CartController(_basketServiceMock.Object, _catalogServiceMock.Object, _identityParserMock.Object);
+        //     orderController.ControllerContext.HttpContext = _contextMock.Object;
+        //     var actionResult = await orderController.AddToCart(fakeCatalogItem);
+
+        //     //Assert
+        //     var redirectToActionResult = Assert.IsType<RedirectToActionResult>(actionResult);
+        //     Assert.Equal("Catalog", redirectToActionResult.ControllerName);
+        //     Assert.Equal("Index", redirectToActionResult.ActionName);
+        // }
+    }
+}
